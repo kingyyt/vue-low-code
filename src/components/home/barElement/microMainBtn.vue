f296c2930e6d11594e0a487c269862be2c9563e6
kingyyt <yeyiteng@163.com>
2024-06-17 22:50:02 +0800

    updata

--- src/components/home/barElement/microMainBtn.vue
+++ src/components/home/barElement/microMainBtn.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { nextTick, onMounted,ref } from 'vue'
-import type {Ref} from 'vue'
+import { nextTick, onMounted, ref } from 'vue'
+import type { Ref } from 'vue'
 import {
   PlayCircleOutlined,
   ReloadOutlined,
@@ -14,12 +14,11 @@ import {
   GetJsonList,
   PostJsonList,
   // GetJsonListDetail,
-  PatchJsonListDetail,
+  PatchJsonListDetail
   // DeleteJsonListDetail
 } from '@/api/microMain/microMain'
-import { message } from 'ant-design-vue';
+import { message } from 'ant-design-vue'
 import { useMainListStore } from '@/stores/modules/microPage'
-
 
 // 初始化
 const init = () => {
@@ -31,70 +30,71 @@ onMounted(async () => {
 // 获取mainListStore
 const store = useMainListStore()
 // 获取json列表
-interface JsonListData{
+interface JsonListData {
   id: number
-  json:any
+  json: any
   name: string
   user_id: number
 }
 let list: Ref<JsonListData[]> = ref([])
-const getJsonList = async ()=> {
+const getJsonList = async () => {
   const res = await GetJsonList()
-  if(res && res.data){
-    list.value = res.data.map((item:JsonListData)=>{
-      try{
+  if (res && res.data) {
+    list.value = res.data.map((item: JsonListData) => {
+      try {
         const parseData = JSON.parse(item.json)
-        return{
+        return {
           ...item,
-          json:parseData
+          json: parseData
         }
+      } catch (error) {
+        return {
+          ...item,
+          json: []
         }
-        catch(error){
-          return{
-            ...item,
-            json:[]
-          }
       }
     })
   }
-  console.log(list.value)
 }
 // 列表点击
-const currentPageList:Ref<JsonListData | null> = ref(null) 
-const openChange = (item:any)=>{
+const currentPageList: Ref<JsonListData | null> = ref(null)
+const openChange = (item: any) => {
   currentPageList.value = item
   store.setMainList(item.json)
   store.setPageName(item.name)
   store.setUpdate(1)
 }
 // 保存页面
-const savePage = async ()=>{
-  if(currentPageList.value && currentPageList.value.id){
-    const res:any = await PatchJsonListDetail({json:listToJson(),name:store.name},currentPageList.value.id)
-    if(!res) return
-    message.success('编辑成功');
+const savePage = async () => {
+  if (currentPageList.value && currentPageList.value.id) {
+    const res: any = await PatchJsonListDetail(
+      { json: listToJson(), name: store.name },
+      currentPageList.value.id
+    )
+    if (!res) return
+    message.success('编辑成功')
     currentPageList.value = res
-    if(currentPageList.value && currentPageList.value.json){
+    if (currentPageList.value && currentPageList.value.json) {
       currentPageList.value.json = JSON.parse(currentPageList.value?.json)
     }
-  }else{
+  } else {
     store.setUpdate(2)
     await nextTick
-    const res:any = await PostJsonList({
-      json:listToJson(),
-      name:store.name
+    const res: any = await PostJsonList({
+      json: listToJson(),
+      name: store.name
     })
-    if(!res.data) return
-    message.success('保存成功');
+    if (!res.data) return
+    message.success('保存成功')
     currentPageList.value = res.data
-    if(currentPageList.value && currentPageList.value.json){
+    if (currentPageList.value && currentPageList.value.json) {
       currentPageList.value.json = JSON.parse(currentPageList.value?.json)
     }
   }
   init()
 }
 // 新建页面
-const newPage = async ()=>{
+const newPage = async () => {
   currentPageList.value = null
   resetPage()
 }
@@ -117,60 +117,74 @@ const listToJson = () => {
 
 // 重置页面
 
-const resetPage = () =>{
+const resetPage = () => {
   store.removeMainList()
   store.removeName()
   store.setUpdate(3)
 }
 
+// 下载源码
+const downCode = async () => {
+  store.setUpdate(2)
+  await nextTick
+
+  console.log(listToJson())
+}
 </script>
 
 <template>
   <div class="dark:text-white">
-  <a-popover >
-    <template #content>
-      <p>预览</p>
-    </template>
-    <PlayCircleOutlined class="mr-4" />
-  </a-popover>
-  <a-popover >
-    <template #content>
-      <p>下载源码</p>
-    </template>
-    <CloudDownloadOutlined class="mr-4" />
-  </a-popover>
-  <a-popover >
-    <template #content>
-      <p>重置</p>
-    </template>
-    <ReloadOutlined @click="resetPage" class="mr-4" />
-  </a-popover>
-  <a-popover >
-    <template #content>
-      <p>保存</p>
-    </template>
-    <SaveOutlined @click="savePage" class="mr-4" />
-  </a-popover>
-  <a-popover >
-    <template #content>
-      <p>新增页面</p>
-    </template>
-    <PlusCircleOutlined @click="newPage"  class="mr-4" />
-  </a-popover>
-  
-  <a-dropdown >
-    <a class="ant-dropdown-link text-sm" @click.prevent>
-      选择页面
-      <DownOutlined />
-    </a>
-    <template #overlay>
-      <a-menu>
-        <a-menu-item v-for="(item,index) in list" :key="index">
-          <div @click="openChange(item)">{{ item.name }} </div>
-        </a-menu-item>
-      </a-menu>
-    </template>
-  </a-dropdown>
-  <span class="ml-4 text-sm"><span class=" text-yellow-400"><InfoCircleOutlined /></span>  {{ currentPageList && currentPageList.id ? `当前编辑页面：${currentPageList.name}`  : '正在编辑新页面' }}</span>
+    <a-popover>
+      <template #content>
+        <p>预览</p>
+      </template>
+      <PlayCircleOutlined class="mr-4" />
+    </a-popover>
+    <a-popover>
+      <template #content>
+        <p>下载源码</p>
+      </template>
+      <CloudDownloadOutlined @click="downCode" class="mr-4" />
+    </a-popover>
+    <a-popover>
+      <template #content>
+        <p>重置</p>
+      </template>
+      <ReloadOutlined @click="resetPage" class="mr-4" />
+    </a-popover>
+    <a-popover>
+      <template #content>
+        <p>保存</p>
+      </template>
+      <SaveOutlined @click="savePage" class="mr-4" />
+    </a-popover>
+    <a-popover>
+      <template #content>
+        <p>新增页面</p>
+      </template>
+      <PlusCircleOutlined @click="newPage" class="mr-4" />
+    </a-popover>
+
+    <a-dropdown>
+      <a class="ant-dropdown-link text-sm" @click.prevent>
+        选择页面
+        <DownOutlined />
+      </a>
+      <template #overlay>
+        <a-menu>
+          <a-menu-item v-for="(item, index) in list" :key="index">
+            <div @click="openChange(item)">{{ item.name }}</div>
+          </a-menu-item>
+        </a-menu>
+      </template>
+    </a-dropdown>
+    <span class="ml-4 text-sm"
+      ><span class="text-yellow-400"><InfoCircleOutlined /></span>
+      {{
+        currentPageList && currentPageList.id
+          ? `当前编辑页面：${currentPageList.name}`
+          : '正在编辑新页面'
+      }}</span
+    >
   </div>
 </template>
--- src/packages/index.ts
+++ src/packages/index.ts
@@ -1,5 +1,8 @@
-export interface componentDefaultAttr {
-  name: string
-  id: string
-  url: string
+// 键为id id值唯一 且与目录下文件相同 值为所在组件类型
+export default {
+  button: 'basic',
+  cell: 'basic',
+  icon: 'basic',
+  cell2: 'business',
+  button2: 'form'
 }
